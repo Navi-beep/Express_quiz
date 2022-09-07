dc4e7410ba90bfc89739dd382a8b7ff82ee5d259
Navi-Beep <estawars30@gmail.com>
2022-09-07 12:07:19 -0500

    fixed stuff

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,14 +2,14 @@ const MainAuthRouter = require('express').Router();
 
 
 MainAuthRouter.route('/register')
-    .get(require('register.view'))
+    .get(require('./register.view'))
     .post((req, res) => { res.send('Post Main Auth Router')
     })
 
 
 
 MainAuthRouter.route('/login')
-    .get(require ('login.view'))
+    .get(require ('./login.view'))
 
 
 
