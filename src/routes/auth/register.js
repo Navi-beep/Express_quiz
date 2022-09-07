de34b2d3be10d989db2e8721af1ac29bed6f93e6
Navi-Beep <estawars30@gmail.com>
2022-09-07 14:44:38 -0500

    added files

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,7 +1,10 @@
+const { User } = require('../../models');
+
 module.exports = (req, res) => {
     if (req.body.password !== req.body.confirmPass){
         res.send({error: 'Your passwords dont match'})
         return 
     }
+    const { username, email, password } = req.body
     res.send('Post register from register')
 }
