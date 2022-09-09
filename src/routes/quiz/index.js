465d14361282caab77b4b97e312cc7f858489ebe
Navi-Beep <estawars30@gmail.com>
2022-09-09 16:49:18 -0500

    added files

---
+++ src/routes/quiz/index.js
@@ -0,0 +1,7 @@
+const QuizDashboardRouter = require('express').Router();
+
+QuizDashboardRouter.route("/create")
+    .get(require('./editor'))
+    .post(require('./create'))
+
+module.exports = QuizDashboardRouter
